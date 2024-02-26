922e3d6001935a8e1cc64c7f1ad343ffd241c2c3
Tanasie Vlad <tanasievladcristian@gmail.com>
2024-02-26 11:57:14 +0200

    Added graphql config to the main module and changed the app.controller spec file to expect 'OK!'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.NODE_ENV === 'development',
+      typePaths: ['./**/*.hraphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
