08e5d8077ab796140e8bb45c8ef499ef4c413187
Tanasie Vlad <tanasievladcristian@gmail.com>
2024-03-06 10:55:01 +0200

    Added index file for commands and command handlers

---
+++ backend/src/accounting/invoice/command/index.ts
@@ -0,0 +1,9 @@
+export { AssignCustomerCommand } from './commands/assign-customer.command';
+export { CreateInvoiceCommand } from './commands/create-invoice.command';
+export { DeleteInvoiceCommand } from './commands/delete-invoice.command';
+export { UpdateInvoiceCommand } from './commands/update-invoice.command';
+export { GenerateStornoCommand } from './commands/generate-storno.command';
+export { MarkInvoiceAsPaidCommand } from './commands/mark-invoice-paid.command';
+export { MarkInvoiceAsUnpaid } from './commands/mark-invoice-unpaid.command';
+
+export { AssignCustomerHandler } from './handlers/assign-customer.handler';
