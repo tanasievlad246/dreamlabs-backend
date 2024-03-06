4f7ef021d24f3ea025d81acf8c38ef60eb501aad
Tanasie Vlad <tanasievladcristian@gmail.com>
2024-03-06 13:48:12 +0200

    Added exception to find query

--- backend/src/accounting/invoice/query/handlers/find-one.query-handler.ts
+++ backend/src/accounting/invoice/query/handlers/find-one.query-handler.ts
@@ -3,6 +3,7 @@ import { Invoice } from '../../domain/invoice.entity';
 import { Repository } from 'typeorm';
 import { FindOneQuery } from '../queries/find-one.query';
 import { QueryHandler } from '@nestjs/cqrs';
+import { NotFoundException } from '@nestjs/common';
 
 @QueryHandler(FindOneQuery)
 export class FindOneQueryHandler {
@@ -10,8 +11,14 @@ export class FindOneQueryHandler {
     @InjectRepository(Invoice) private readonly repository: Repository<Invoice>,
   ) {}
   async execute(query: FindOneQuery) {
-    return await this.repository.findOneBy({
+    const invoice = await this.repository.findOneBy({
       id: query.id,
     });
+
+    if (!invoice) {
+      throw new NotFoundException('Invoice not found');
+    }
+
+    return invoice;
   }
 }
