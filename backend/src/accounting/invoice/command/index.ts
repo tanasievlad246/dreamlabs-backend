98a5be82ce9c17d5c38c4afe8c3a0ca2007c35ba
Tanasie Vlad <tanasievladcristian@gmail.com>
2024-03-06 11:04:47 +0200

    Updated index.ts to properly export CommandHandlers

--- backend/src/accounting/invoice/command/index.ts
+++ backend/src/accounting/invoice/command/index.ts
@@ -1,3 +1,11 @@
+import { AssignCustomerHandler } from './handlers/assign-customer.handler';
+import { CreateInvoiceHandler } from './handlers/create-invoice.handler';
+import { DeleteInvoiceHandler } from './handlers/delete-invoice.handler';
+import { UpdateInvoiceHandler } from './handlers/update-invoice.handler';
+import { GenerateStornoHandler } from './handlers/generate-storno.handler';
+import { MarkInvoiceAsPaidHandler } from './handlers/mark-paid.handler';
+import { MarkInvoiceAsUnpaidHandler } from './handlers/mark-unpaid.handler';
+
 export { AssignCustomerCommand } from './commands/assign-customer.command';
 export { CreateInvoiceCommand } from './commands/create-invoice.command';
 export { DeleteInvoiceCommand } from './commands/delete-invoice.command';
@@ -7,3 +15,19 @@ export { MarkInvoiceAsPaidCommand } from './commands/mark-invoice-paid.command';
 export { MarkInvoiceAsUnpaid } from './commands/mark-invoice-unpaid.command';
 
 export { AssignCustomerHandler } from './handlers/assign-customer.handler';
+export { CreateInvoiceHandler } from './handlers/create-invoice.handler';
+export { DeleteInvoiceHandler } from './handlers/delete-invoice.handler';
+export { UpdateInvoiceHandler } from './handlers/update-invoice.handler';
+export { GenerateStornoHandler } from './handlers/generate-storno.handler';
+export { MarkInvoiceAsPaidHandler } from './handlers/mark-paid.handler';
+export { MarkInvoiceAsUnpaidHandler } from './handlers/mark-unpaid.handler';
+
+export const CommandHandlers = [
+  AssignCustomerHandler,
+  CreateInvoiceHandler,
+  DeleteInvoiceHandler,
+  UpdateInvoiceHandler,
+  GenerateStornoHandler,
+  MarkInvoiceAsPaidHandler,
+  MarkInvoiceAsUnpaidHandler,
+];
